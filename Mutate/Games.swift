94f63ccb0042061d6baeda3420240ef807edd56a
mark lucking <mark.lucking@gmail.com>
2021-08-26 22:40:19 +0200

    Continued to investigate GameKit

--- Mutate/Games.swift
+++ Mutate/Games.swift
@@ -10,10 +10,14 @@ import GameKit
 import UIKit
 import SwiftUI
 
-class SimpleViewController: UIViewController, UINavigationControllerDelegate, GKGameCenterControllerDelegate, GKLocalPlayerListener {
+var vc:GKMatchmakerViewController!
 
-  let minPlayers = 1
+class SimpleViewController: UIViewController, GKLocalPlayerListener {
+
+  let minPlayers = 2
   let maxPlayers = 4
+  
+  
   
   @objc func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
     // do nothing
@@ -26,20 +30,44 @@ class SimpleViewController: UIViewController, UINavigationControllerDelegate, GK
   override func viewDidLoad() {
       super.viewDidLoad()
       
-    GKLocalPlayer.local.authenticateHandler = { [self] gcAuthVC, error in
-        if GKLocalPlayer.local.isAuthenticated {
-          GKLocalPlayer.local.register(self)
-          print("Authenticated to Game Center!")
+      GKLocalPlayer.local.authenticateHandler = { [self] gcAuthVC, error in
+      if GKLocalPlayer.local.isAuthenticated {
+        GKLocalPlayer.local.register(self)
+        print("Authenticated to Game Center!")
 
-          let request = GKMatchRequest()
-          request.minPlayers = minPlayers
-          request.maxPlayers = maxPlayers
-          request.inviteMessage = "Mutant Anyone?"
+//        GKMatchmaker.shared().cancel()
+        
+        let request = GKMatchRequest()
+        request.minPlayers = minPlayers
+        request.maxPlayers = maxPlayers
+        request.inviteMessage = "Mutant Anyone?"
+        request.defaultNumberOfPlayers = 2
+        request.recipientResponseHandler = { player, response in
+          print("accepted")
+        }
+        
+        vc = GKMatchmakerViewController(matchRequest: request)
+        vc.matchmakerDelegate = self
+//        vc!.delegate = self
+        present(vc, animated: true)
           
-          let vc = GKMatchmakerViewController(matchRequest: createRequest())
-          vc!.delegate = self
-          present(vc!, animated: true)
+//        GameCenter is Apple's social gaming network!
+//          game discoverability, players profiles
+
           
+
+
+//          Friends API iOS 14 Add friends button on screen [use SF symbols]
+//          Fast Start iOS 14
+//          competition or connection
+
+//
+
+
+//        Support several types of multiplayer games
+//          realtime, turnbased and server hosted
+//            mutlple ways to find other players, auto-match, friends, contacts, recently played, nearby, game players groups, message groups
+//              works cross platform mac & iphone & ipad & appleTV
           
 //          let vc = GKTurnBasedMatchmakerViewController(matchRequest: request)
 //          vc.delegate = self
@@ -91,14 +119,16 @@ class SimpleViewController: UIViewController, UINavigationControllerDelegate, GK
 //              var achievedDescription: String { get set }
 //            + APIs for images achived + failed
         
-        } else if let vc = gcAuthVC {
-          self.present(vc, animated: true)
-        }
-        else {
-          print("Error authentication to GameCenter: " +
-            "\(error?.localizedDescription ?? "none")")
-        }
+      } else if let vc = gcAuthVC {
+        self.present(vc, animated: true)
       }
+      else {
+        print("Error authentication to GameCenter: " +
+          "\(error?.localizedDescription ?? "none")")
+      }
+    }
+    
+    
       
   }
   
@@ -114,50 +144,86 @@ class SimpleViewController: UIViewController, UINavigationControllerDelegate, GK
 }
 
 
-extension SimpleViewController: GKMatchmakerViewControllerDelegate {
-
-  
-
-  private func createMatchmaker(withInvite invite: GKInvite? = nil) -> GKMatchmakerViewController? {
-        
-        //If there is an invite, create the matchmaker vc with it
-        if let invite = invite {
-            return GKMatchmakerViewController(invite: invite)
-        }
-        
-        return GKMatchmakerViewController(matchRequest: createRequest())
-    }
+extension SimpleViewController: GKMatchmakerViewControllerDelegate, GKMatchDelegate, UINavigationControllerDelegate, GKGameCenterControllerDelegate, GKInviteEventListener, GKTurnBasedEventListener {
     
-    private func createRequest() -> GKMatchRequest {
-        print("new request")
-        let request = GKMatchRequest()
-        request.minPlayers = minPlayers
-        request.maxPlayers = maxPlayers
-        request.inviteMessage = "Mutant Anyone?"
-        return request
-    }
+  func player(_ player: GKPlayer, didRequestMatchWithOtherPlayers playersToInvite: [GKPlayer]) {
+    print("------------------")
+  }
     
-    func matchmakerViewController(_ viewController: GKMatchmakerViewController, didFind match: GKMatch) {
-        viewController.dismiss(animated: true)
-        print("matchmakerViewController")
-//        delegate?.presentGame(match: match)
+    func player(_ player: GKPlayer, didRequestMatchWithRecipients recipientPlayers: [GKPlayer]) {
+      print("++++++++++++++++++++")
     }
     
     func player(_ player: GKPlayer, didAccept invite: GKInvite) {
+//      GKMatchmaker.shared().findPlayers(forHostedRequest: <#T##GKMatchRequest#>) { [], error in
+//        <#code#>
+//      }
+//      GKMatchmaker.shared().addPlayers(to: <#T##GKMatch#>, matchRequest: <#T##GKMatchRequest#>) { error in
+//        <#code#>
+//      }
+      
         self.dismiss(animated: true, completion: {
-            print("player ",invite)
-//            self.presentMatchmaker(withInvite: invite)
+            print("foobar", player,invite)
+//            GKLocalPlayer.local.register(self)
+//
+//
+//            let vc = GKMatchmakerViewController(invite: invite)
+//            vc?.delegate = self
+//            self.present(vc!, animated: true)
+        })
+    }
+    
+   func matchmakerViewController(_ viewController: GKMatchmakerViewController, didFind match: GKMatch) {
+        self.dismiss(animated: true)
+        match.delegate = self
+        
+        print("matchmakerViewController ")
+
+    }
+  
+    func matchmakerViewController(_ viewController: GKMatchmakerViewController, hostedPlayerDidAccept player: GKPlayer) {
+      print("matchmakerViewController")
+      self.dismiss(animated: true, completion: {
+            print("player ",player)
+        })
+    }
+    
+    func matchmakerViewController(_ viewController: GKMatchmakerViewController, didFindHostedPlayers players: [GKPlayer]) {
+      print("matchmakerViewController")
+      self.dismiss(animated: true, completion: {
+            print("player ",players)
         })
     }
     
     func matchmakerViewControllerWasCancelled(_ viewController: GKMatchmakerViewController) {
-        viewController.dismiss(animated: true)
+        self.dismiss(animated: true)
         print("matchmakerViewControllerWasCancelled")
     }
     
     func matchmakerViewController(_ viewController: GKMatchmakerViewController, didFailWithError error: Error) {
         print("Matchmaker vc did fail with error: \(error.localizedDescription).")
     }
+    
+  func match(_ match: GKMatch, player: GKPlayer, didChange state: GKPlayerConnectionState) {
+    print("match")
+  }
+  
+  func match(_ match: GKMatch, didFailWithError error: Error?) {
+    print("didFailWithError")
+  }
+  
+  func match(_ match: GKMatch, shouldReinviteDisconnectedPlayer player: GKPlayer) -> Bool {
+    print("shouldReinviteDisconnectedPlayer")
+    return true
+  }
+  
+  func match(_ match: GKMatch, didReceive data: Data, fromRemotePlayer player: GKPlayer) {
+    print("didReceive")
+  }
+  
+  func match(_ match: GKMatch, didReceive data: Data, forRecipient recipient: GKPlayer, fromRemotePlayer player: GKPlayer) {
+    print("didReceive")
+  }
     
 }
 
